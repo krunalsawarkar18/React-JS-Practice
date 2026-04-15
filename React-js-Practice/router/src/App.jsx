4ddd3048dcafb53cdc91d4f82fe1707fb308e5e3
krunalsawarkar18 <krunalsawarkar2004@gmail.com>
2026-04-15 14:53:14 +0530

    React-js-Practice

---
+++ React-js-Practice/router/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './Navbar'
+import Home from './home';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <div >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App
